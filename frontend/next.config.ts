f64153aedc84f975c14c47f19ac8326403ee4c24
Timofey <timofey.syr17@gmail.com>
2025-07-03 11:03:57 +0300

    add color variables to tailwind config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/v1'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http', // для локал девеломпента
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
