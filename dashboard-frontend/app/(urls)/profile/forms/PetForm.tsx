768550d8a8f9bdde6099a6afdbddf79b68d0dc32
Timofey <timofey.syr17@gmail.com>
2025-08-21 17:31:35 +0300

    profile/pet UI

--- dashboard-frontend/app/(urls)/profile/forms/PetForm.tsx
+++ dashboard-frontend/app/(urls)/profile/forms/PetForm.tsx
@@ -1,7 +1,166 @@
 import React from 'react'
+import { useForm } from '@/app/hooks/useForm'
+import TextInput from '@/components/ui/TextInput'
+import Button from '@/components/ui/Button'
+import showToast from '@/components/ui/showToast'
+import Image from 'next/image'
+
+const validationRules = {
+  imageURL: { required: false },
+  name: { required: true },
+  type: { required: true },
+  birthday: { required: true },
+  gender: { required: true },
+  breed: { required: true },
+  color: { required: false },
+  comment: { required: false },
+  allergies: { required: false },
+}
 
 const PetForm = () => {
-  return <div className="py-4">PetForm</div>
+  const { values, handleChange, handleSubmit } = useForm(
+    {
+      imageURL: '',
+      name: '',
+      type: '',
+      birthday: '',
+      gender: '',
+      breed: '',
+      color: '',
+      comment: '',
+      allergies: '',
+      QR_code: '',
+    },
+    validationRules,
+    async values => {
+      try {
+        const response = await fetch('/api/profile/contacts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ values }),
+        })
+
+        if (!response.ok) {
+          const error = await response.json()
+          throw new Error(error.error || 'Ошибка при обновлении данных')
+        }
+
+        const result = await response.json()
+        showToast({ type: 'success', message: 'Данные успешно обновлены!' })
+      } catch (error) {
+        showToast({
+          type: 'error',
+          message: error instanceof Error ? error.message : 'Ой, что то пошло не так..',
+        })
+      }
+    }
+  )
+  return (
+    <div className="space-y-3 py-3">
+      <div className="overflow-hidden rounded-2xl pl-1">
+        <div className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div className="flex items-center justify-around gap-4 py-4">
+              <Image
+                src={values.imageURL || '/images/noPet.svg'}
+                alt="Pet"
+                width={160}
+                height={160}
+                className="rounded-full object-cover hover:cursor-pointer"
+              />
+              <Image
+                src={values.QR_code || '/images/noQR.svg'}
+                alt="qrcode"
+                width={160}
+                height={160}
+                className="rounded-full object-cover hover:cursor-pointer"
+              />
+            </div>
+            <div className="h-[2px] w-full bg-white" />
+            <div className="grid grid-cols-1 gap-6 pb-4 md:grid-cols-3">
+              <TextInput
+                name="name"
+                value={values.name}
+                handleChange={handleChange}
+                label="Кличка"
+                placeholder="Как зовут вашего питомца?"
+                style="register"
+              />
+              <TextInput
+                name="type"
+                value={values.type}
+                handleChange={handleChange}
+                label="Вид"
+                placeholder="Какой у вас питомец?"
+                style="register"
+              />
+              <TextInput
+                name="birthday"
+                value={values.birthday}
+                handleChange={handleChange}
+                label="Дата рождения"
+                placeholder="Дата рождения"
+                style="register"
+              />
+              <TextInput
+                name="gender"
+                value={values.gender}
+                handleChange={handleChange}
+                label="Пол"
+                placeholder="Пол"
+                style="register"
+              />
+              <TextInput
+                name="breed"
+                value={values.breed}
+                handleChange={handleChange}
+                label="Порода"
+                placeholder="Порода"
+                style="register"
+              />
+              <TextInput
+                name="color"
+                value={values.color}
+                handleChange={handleChange}
+                label="Цвет"
+                placeholder="Цвет"
+                style="register"
+              />
+            </div>
+
+            <div className="grid grid-cols-1 gap-6 pb-4 md:grid-cols-2">
+              <TextInput
+                name="comment"
+                value={values.comment}
+                handleChange={handleChange}
+                label="Комментарий"
+                placeholder="Комментарий"
+                style="register"
+              />
+              <TextInput
+                name="allergies"
+                value={values.allergies}
+                handleChange={handleChange}
+                label="Аллергии"
+                placeholder="Аллергии"
+                style="register"
+              />
+            </div>
+
+            <div className="flex items-center justify-center">
+              <Button
+                text="Сохранить"
+                className="bg-orange mt-4 flex w-full items-center justify-center text-white"
+                type="submit"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PetForm
